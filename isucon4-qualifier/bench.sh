4e001bf8edbd11ce2d7db6c1e441c1ebeb8a297a
Fujimisakari <fujimisakari@gmail.com>
2017-06-25 22:16:46 +0900

    sqlを変更できるよう対応

--- isucon4-qualifier/bench.sh
+++ isucon4-qualifier/bench.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --link isu4_qualifier_mysql:mysql --link isu4_qualifier_nginx:nginx isu4_qualifier_bench /home/isucon/benchmarker bench --host nginx
+docker run -it -v `pwd`/sql:/home/isucon/sql --link isu4_qualifier_mysql:mysql --link isu4_qualifier_nginx:nginx isu4_qualifier_bench /home/isucon/benchmarker bench --host nginx
